f304a84eb24ddb6ea30d26ff03b3024eecc95316
Ivan Maximov <60979535+ivan-kud@users.noreply.github.com>
2023-01-11 09:58:07 +0000

    move html files

--- app/src/main.py
+++ app/src/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.src import digit
+from . import digit
 
 
 app = FastAPI()
--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -2,4 +2,14 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
-pip install tqdm numpy pandas matplotlib scikit-learn tensorboard jupyterlab ipywidgets lckr-jupyterlab-variableinspector torch-lr-finder
+pip install \
+    tqdm \
+    numpy \
+    pandas \
+    matplotlib \
+    scikit-learn \
+    tensorboard \
+    jupyterlab \
+    ipywidgets \
+    lckr-jupyterlab-variableinspector \
+    torch-lr-finder
