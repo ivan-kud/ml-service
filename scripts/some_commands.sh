bbc0797bc940561146876cd4a6ada847a3d04bd3
Ivan Maximov <ivan.kudashkin@gmail.com>
2023-01-29 18:56:24 +0300

    add files for local deployment

--- scripts/some_commands.sh
+++ scripts/some_commands.sh
@@ -20,23 +20,6 @@ docker network create traefik-public
 cd ml-service
 docker compose up -d
 
-#################################################
-# B e f o r e   D r o p l e t   d e l e t i o n #
-#################################################
-# SSH to host as 'root'
-ssh root@ivankud.com
-# Add environment variables
-export TRAEFIK_USERNAME=xxxxxxxx
-export TRAEFIK_PASSWORD=xxxxxxxx
-export TRAEFIK_HASHED_PASSWORD=$(openssl passwd -apr1 $TRAEFIK_PASSWORD)
-# Down Compose
-cd ml-service
-docker compose down
-# Find certificates on remote server (optional)
-find / -type f -iname "acme.json"
-# Pull certificates to local machine (command should be executed on the local machine)
-rsync -a root@ivankud.com:/var/lib/docker/volumes/ml-service_traefik-public-certificates/_data/ ~/Documents/GitHub/ml-service/certificates
-
 #################################
 # U s e f u l   C o m m a n d s #
 #################################
@@ -45,7 +28,7 @@ cd app
 uvicorn src.app:app --reload
 
 # Build docker image
-docker build . -t ivankud/ml-service
+docker build ./ -f Dockerfile-locally -t ivankud/ml-service
 # Run docker container
 docker run --rm -it -p 80:80 ivankud/ml-service
 # Login to Docker Hub
@@ -58,14 +41,14 @@ docker push ivankud/ml-service
 docker pull ivankud/ml-service:latest
 
 # Docker Compose
-docker compose up -d
+docker compose -f compose-locally.yaml up -d
 docker compose ps
 docker compose logs
 docker compose pause
 docker compose unpause
 docker compose stop
 docker compose start
-docker compose down
+docker compose -f compose-locally.yaml down
 
 # Examine the list of installed UFW profiles
 ufw app list
@@ -86,6 +69,10 @@ usermod -aG sudo ml-service
 rsync -a --chown=ml-service:ml-service ~/.ssh /home/ml-service
 # Now, open up a new terminal session, and use SSH to log in as new user
 ssh ml-service@ivankud.com
+
+# Find and pull certificates to local machine (command should be executed on the local machine)
+find / -type f -iname "acme.json"
+rsync -a root@ivankud.com:/var/lib/docker/volumes/ml-service_traefik-public-certificates/_data/ ~/Documents/GitHub/ml-service/certificates
 
 # Run JupyterLab
 jupyter-lab
