7e5d70e2a7b801ed5ba7e582f16ac4aa88d93465
Ivan Maximov <ivan.kudashkin@gmail.com>
2023-01-29 15:39:20 +0300

    change scripts

--- scripts/some_commands.sh
+++ scripts/some_commands.sh
@@ -1,9 +1,45 @@
-# Run JupyterLab
-jupyter-lab
+###############################################
+# A f t e r   D r o p l e t   c r e a t i o n #
+###############################################
+# Change DNS records to new IP
+# SSH to host as 'root'
+ssh root@ivankud.com
+# or (if DNS records is still not updated)
+ssh root@xxx.xxx.xxx.xxx
+# Install updates (optional)
+apt update && apt upgrade
+# Clone repository
+git clone https://github.com/ivan-kud/ml-service.git
+# Add environment variables
+export TRAEFIK_USERNAME=xxxxxxxx
+export TRAEFIK_PASSWORD=xxxxxxxx
+export TRAEFIK_HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
+# Create network
+docker network create traefik-public
+# Build images and start containers
+cd ml-service
+docker compose up -d
 
-# Run TensorBoard
-tensorboard --logdir=runs
+#################################################
+# B e f o r e   D r o p l e t   d e l e t i o n #
+#################################################
+# SSH to host as 'root'
+ssh root@ivankud.com
+# Add environment variables
+export USERNAME=xxxxxxxx
+export PASSWORD=xxxxxxxx
+export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
+# Down Compose
+cd ml-service
+docker compose down
+# Find certificates on remote server (optional)
+find / -type f -iname "acme.json"
+# Pull certificates to local machine (command should be executed on the local machine)
+rsync -a root@ivankud.com:/var/lib/docker/volumes/ml-service_traefik-public-certificates/_data/ ~/Documents/GitHub/ml-service/certificates
 
+#################################
+# U s e f u l   C o m m a n d s #
+#################################
 # Run application locally
 cd app
 uvicorn src.app:app --reload
@@ -47,32 +83,12 @@ adduser ml-service
 # Add your new user to the sudo group
 usermod -aG sudo ml-service
 # Copy your local public SSH key to new user to log in with SSH
-rsync --archive --chown=ml-service:ml-service ~/.ssh /home/ml-service
+rsync -a --chown=ml-service:ml-service ~/.ssh /home/ml-service
 # Now, open up a new terminal session, and use SSH to log in as new user
-ssh ml-service@xxx.xxx.xxx.xxx
+ssh ml-service@ivankud.com
 
-###############################################
-# A f t e r   D r o p l e t   c r e a t i o n #
-###############################################
-# SSH to host as 'root'
-ssh root@ivankud.com
-# Install updates (optional)
-apt-get update && apt-get upgrade
-# Clone repository
-git clone https://github.com/ivan-kud/ml-service.git
-# Go to directory
-cd ml-service
-# Add environment variables
-export USERNAME=xxxxxx
-export PASSWORD=xxxxxxxx
-export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
-# Create network
-docker network create traefik-public
-# Build images and start containers
-docker compose up -d
+# Run JupyterLab
+jupyter-lab
 
-#################################################
-# B e f o r e   D r o p l e t   d e l e t i o n #
-#################################################
-docker compose down
-rm -r ml-service
+# Run TensorBoard
+tensorboard --logdir=runs
